b4d5847b74c6afb034792015c6e75abe5667535a
Muhammad Irsal FajriEfendy <64578833+Muhammad-Irsal@users.noreply.github.com>
2020-05-16 15:10:03 +0800

    Create Jobsheet 7 C++.cpp
    Mengisi elemen array menggunakan perulangan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main (){
+int nim [8];
+cout<<"Muhammad Irsal FajriEfendy-F1B019097\n\n";
+for (int i = 0; i<= 8; i++){
+cout<< "Masukkan nimke-" << i+1;
+cin>>nim[i];
+}
+}
+
