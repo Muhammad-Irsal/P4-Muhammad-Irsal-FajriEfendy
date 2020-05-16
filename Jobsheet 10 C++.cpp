d970a121545bfc140aa13deb28d01ff852ed18f0
Muhammad Irsal FajriEfendy <64578833+Muhammad-Irsal@users.noreply.github.com>
2020-05-16 15:13:17 +0800

    Create Jobsheet 10 C++.cpp
    Array 3 dimensi

---
+++ Jobsheet 10 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main (){
+char nama [2][2][10];
+int i, j;
+cout<<"Muhammad Irsal FajriEfendy-F1B019097\n\n";
+for (i = 0; i< 2; i++){
+for (j = 1; j < 10; j++){
+cout<< "ISI APA AJA  [" <<i<<"][" << j << "] = ";
+cin>>nama[i][j];
+}
+cout<<endl;
+}
+}
