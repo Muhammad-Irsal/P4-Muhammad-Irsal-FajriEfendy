6092a74362de9559311d42fc8f14fa821541caaa
Muhammad Irsal FajriEfendy <64578833+Muhammad-Irsal@users.noreply.github.com>
2020-05-16 15:10:35 +0800

    Create Jobsheet 8 C++.cpp
     Mengisi dan menampilkan Elemen Array menggunakan perulangan

---
+++ Jobsheet 8 C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+int x[8],i;
+cout<<"Muhammad Irsal FajriEfendy-F1B019097\n\n";
+cout<<"masukkan NIM terakhir Anda"<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"Masukkan nim "<<i+1<<" : ";
+cin>>x[i];
+}
+cout<<endl;
+for(i=0;i<=4;i++)
+{
+cout<<"nim ke- "<<i+1<<" = "<<x[i]<<endl;
+} cout<<endl;
+
+return 0 ;
+}
