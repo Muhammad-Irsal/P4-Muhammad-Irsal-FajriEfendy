fd64cc53f2993d48b03791045893d8e9dcaf7434
Muhammad Irsal FajriEfendy <64578833+Muhammad-Irsal@users.noreply.github.com>
2020-05-16 15:05:58 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Muhammad Irsal FajriEfendy-F1B019097\n\n";
+int nim[6]= {0,1,9,0,9,7};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+
+	
+
+}
