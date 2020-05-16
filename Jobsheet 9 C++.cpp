883b13e6ff375f125ed7c4156ebab088fd3e14ab
Muhammad Irsal FajriEfendy <64578833+Muhammad-Irsal@users.noreply.github.com>
2020-05-16 15:12:35 +0800

    Create Jobsheet 9 C++.cpp
    Array sebagai karakter

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+int i;
+cout<<"Muhammad Irsal FajriEfendy-F1B019097\n\n";
+char nama[5]={'I','R','S','A','L'};
+char nama2[6]="IRSAL";
+cout<<"Array per Karakter= ";
+for(i=0;i<=4;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
